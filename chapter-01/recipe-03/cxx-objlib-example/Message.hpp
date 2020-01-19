ae74df3a320b235e302ae9123b6723d0f24fa265
hexu <hexu@sensetime.com>
2020-01-19 16:37:29 +0800

    rm symlink of chapter-01/recipe-03/cxx-objlib-example

--- chapter-01/recipe-03/cxx-objlib-example/Message.hpp
+++ chapter-01/recipe-03/cxx-objlib-example/Message.hpp
@@ -1,1 +1,17 @@
-../cxx-example/Message.hpp
+#pragma once
+
+#include <iosfwd>
+#include <string>
+
+class Message {
+public:
+  Message(const std::string &m) : message_(m) {}
+
+  friend std::ostream &operator<<(std::ostream &os, Message &obj) {
+    return obj.printObject(os);
+  }
+
+private:
+  std::string message_;
+  std::ostream &printObject(std::ostream &os);
+};
--- chapter-01/recipe-03/cxx-objlib-example/hello-world.cpp
+++ chapter-01/recipe-03/cxx-objlib-example/hello-world.cpp
@@ -1,1 +1,16 @@
-../cxx-example/hello-world.cpp
+#include "Message.hpp"
+
+#include <cstdlib>
+#include <iostream>
+
+int main() {
+  Message say_hello("Hello, CMake World!");
+
+  std::cout << say_hello << std::endl;
+
+  Message say_goodbye("Goodbye, CMake World");
+
+  std::cout << say_goodbye << std::endl;
+
+  return EXIT_SUCCESS;
+}
